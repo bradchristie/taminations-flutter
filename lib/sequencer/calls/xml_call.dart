a011958ccac64606a239da1c35b88462a3b187a9
Brad Christie <brad@bradchristie.com>
2020-11-17 21:13:45 -0800

    Code for XMLCall

--- lib/sequencer/calls/xml_call.dart
+++ lib/sequencer/calls/xml_call.dart
@@ -20,8 +20,12 @@
 
 import 'package:xml/xml.dart';
 
+import '../../dancer.dart';
+import '../../tam_utils.dart';
 import '../call_context.dart';
 import 'call.dart';
+import '../../extensions.dart';
+import '../../math/path.dart';
 
 class XMLCall extends Call {
 
@@ -31,5 +35,60 @@ class XMLCall extends Call {
 
   XMLCall(this.xelem, this.xmlmap, this.ctx2)
       : super(xelem.getAttribute("title"));
+
+  @override
+  Future<void> performCall(CallContext ctx, [int i = 0]) async {
+    var allPaths = xelem.childrenNamed("path").map(
+            (element) => Path(TamUtils.translatePath(element))).toList();
+    //  If moving just some of the dancers,
+    //  see if we can keep them in the same shape
+    if (ctx.actives.length < ctx.dancers.length) {
+      //  No animations have been done on ctx2,
+      //  so dancers are still at the start points
+      var ctx3 = CallContext.fromContext(ctx2);
+      //  So ctx3 is a copy of the start point
+      //  Now add the paths
+      for (var ii = 0; ii < ctx3.dancers.length; ii++) {
+        ctx3.dancers[ii].path.add(allPaths[ii >> 1]);
+      }
+      //  And move it to the end point
+      ctx3.extendPaths();
+      ctx3.analyze();
+    }
+
+    var matchResult = ctx.computeFormationOffsets(ctx2, xmlmap, delta: 0.2);
+
+    for (var i3 = 0; i3 < xmlmap.length; i3++) {
+      var m = xmlmap[i3];
+      var p = Path.fromPath(allPaths[m >> 1]);
+      if (p.movelist.isEmpty)
+        p.add(TamUtils.getMove("Stand"));
+      //  Scale active dancers to fit the space they are in
+      //  Compute difference between current formation and XML formation
+      var vd = matchResult.offsets[i3].rotate(-ctx.actives[i3].tx.angle);
+      //  Apply formation difference to first movement of XML path
+      if (vd.length > 0.1)
+        p.skewFirst(-vd.x, -vd.y);
+      //  Add XML path to dancer
+      ctx.actives[i3].path.add(p);
+      //  Move dancer to end so any subsequent modifications (e.g. roll)
+      //  use the new position
+      ctx.actives[i3].animateToEnd();
+    }
+
+    //  Mark dancers that had no XML move as inactive
+    //  Needed for post-call modifications e.g. spread
+    var inactives = <Dancer>[];
+    for (var i4 = 0; i4 < xmlmap.length; i4++) {
+      var m = xmlmap[i4];
+      if (allPaths[m >> 1].movelist.length == 0)
+        inactives.add(ctx.actives[i4]);
+    }
+    inactives.forEach((d) {
+      d.data.active = false;
+    });
+  }
+
+
 
 }
