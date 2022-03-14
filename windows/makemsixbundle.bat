ce55006e4b1be7156c89290dd6e87b8479996e0f
Brad Christie <brad@bradchristie.com>
2022-03-14 14:01:44 -0700

    Add Windows files, fix CPU usage

---
+++ windows/makemsixbundle.bat
@@ -0,0 +1,8 @@
+REM flutter build windows
+REM flutter pub run msix:create
+REM mkdir c:\temp\unpack
+REM makeappx unpack /d c:\temp\unpack /p p:\FlutterProjects\taminations\build\windows\runner\Release\taminations.msix
+REM edit manifest file
+REM makeappx pack /d c:\temp\unpack /p p:\FlutterProjects\taminations\build\windows\runner\Release\taminations.msix
+REM rmdir /s/q c:\temp\unpack
+makeappx.exe bundle  /f mapping.txt /p taminations.msixbundle /bv 1.6.37.0
