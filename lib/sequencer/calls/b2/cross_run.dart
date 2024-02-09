cb027efd5f598ecd7205609a8977cfec30507dd4
Brad Christie <brad@bradchristie.com>
2024-02-09 11:27:44 -0800

    Fix for Cross Run as a second call on one line

--- lib/sequencer/calls/b2/cross_run.dart
+++ lib/sequencer/calls/b2/cross_run.dart
@@ -85,20 +85,20 @@ class CrossRun extends Action with ActivesOnly {
           //  If centers are running and facing same direction,
           //  dancer on right goes in front (half-sashay action)
           if (d.data.center && ctx.dancerToRight(d)!.angleFacing.isAround(d.angleFacing)) {
-            d.path = DodgeRight.scale(1.0, 0.5).changeBeats(1.0) +
+            d.path += DodgeRight.scale(1.0, 0.5).changeBeats(1.0) +
                 RunRight.scale(1.0, dist / 2).skew(0.0, 1.0);
           } else {
             //  Pass right shoulders
             var scaleX = 1.0;
             if (d.data.end && numright > 2 && dright[2].angleFacing.isAround(d.angleFacing))
               scaleX = 2.0;
-            d.path = RunRight.scale(scaleX, dist / 2);
+            d.path += RunRight.scale(scaleX, dist / 2);
           }
         } else if (numleft == 2 || numleft == 3 || numleft == 6 || numleft == 7) {
           final d2 = dleft[1];
           if (!runners.contains(d2)) realDodgers.add(d2);
           final dist = d.distanceTo(d2);
-          d.path = RunLeft.scale(1.0, dist / 2);
+          d.path += RunLeft.scale(1.0, dist / 2);
         } else
           throw CallError('Error calculating Cross Run');
       }
@@ -114,13 +114,13 @@ class CrossRun extends Action with ActivesOnly {
         var dback = ctx.dancerInBack(d);
         //  Dodge or move forward/back to that spot
         if (runners.contains(dright))
-          d.path = DodgeRight.scale(1.0, d.distanceTo(dright!) / 2);
+          d.path += DodgeRight.scale(1.0, d.distanceTo(dright!) / 2);
         else if (runners.contains(dleft))
-          d.path = DodgeLeft.scale(1.0, d.distanceTo(dleft!) / 2);
+          d.path += DodgeLeft.scale(1.0, d.distanceTo(dleft!) / 2);
         else if (runners.contains(dfront))
-          d.path = Forward.changeBeats(3.0).scale(d.distanceTo(dfront!), 1.0);
+          d.path += Forward.changeBeats(3.0).scale(d.distanceTo(dfront!), 1.0);
         else if (runners.contains(dback))
-          d.path = Back.changeBeats(3.0).scale(d.distanceTo(dback!), 1.0);
+          d.path += Back.changeBeats(3.0).scale(d.distanceTo(dback!), 1.0);
         else
           throw CallError('Unable to calculate Cross Run for dancer $d');
       }
