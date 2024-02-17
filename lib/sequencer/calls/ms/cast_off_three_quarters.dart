0a44ec8d565a8e0e6da34d3483776a05f37d0445
Brad Christie <brad@bradchristie.com>
2024-02-17 09:42:03 -0800

    Fix for <selector> Cast Off 3/4

--- lib/sequencer/calls/ms/cast_off_three_quarters.dart
+++ lib/sequencer/calls/ms/cast_off_three_quarters.dart
@@ -20,7 +20,7 @@
 
 import '../common.dart';
 
-class CastOffThreeQuarters extends Action with ActivesOnly {
+class CastOffThreeQuarters extends Action {
 
   @override var level = LevelData.MS;
   @override var help = '''Cast Off (fraction)
@@ -30,56 +30,66 @@ class CastOffThreeQuarters extends Action with ActivesOnly {
   CastOffThreeQuarters(String name) : super(name);
 
   @override
-  void performCall(CallContext ctx) {
-    var waveDancers = ctx.actives.where((d) => ctx.isInWave(d)).toList();
-    var couplesLeft = ctx.actives.where((d) =>
-    ctx.isInCouple(d) && d.isCenterLeft &&
-        d.data.partner!.isActive && d.data.partner!.isCenterLeft).toList();
-    var couplesRight = ctx.actives.where((d) =>
-    ctx.isInCouple(d) && d.isCenterRight &&
-        d.data.partner!.isActive && d.data.partner!.isCenterRight).toList();
+  void performCall(CallContext ctx0) {
+    ctx0.activesContext((ctx) {
+      var waveDancers = ctx.actives.where((d) => ctx.isInWave(d)).toList();
+      var couplesLeft = ctx.actives
+          .where((d) =>
+              ctx.isInCouple(d) &&
+              d.isCenterLeft &&
+              d.data.partner!.isActive &&
+              d.data.partner!.isCenterLeft)
+          .toList();
+      var couplesRight = ctx.actives
+          .where((d) =>
+              ctx.isInCouple(d) &&
+              d.isCenterRight &&
+              d.data.partner!.isActive &&
+              d.data.partner!.isCenterRight)
+          .toList();
 
-    //  Dancers in mini-waves hinge three times
-    if (waveDancers.isNotEmpty) {
-      ctx.subContext(waveDancers, (ctx2) {
-        if (norm.endsWith('14'))
-          ctx2.applyCalls('Hinge');
-        else if (norm.endsWith('12'))
-          ctx2.applyCalls('Hinge','Hinge');
-        else
-          ctx2.applyCalls('Hinge','Hinge','Hinge');
-      });
-    }
+      //  Dancers in mini-waves hinge three times
+      print('Wave dancers: $waveDancers');
+      if (waveDancers.isNotEmpty) {
+        ctx.subContext(waveDancers, (ctx2) {
+          if (norm.endsWith('14'))
+            ctx2.applyCalls('Hinge');
+          else if (norm.endsWith('12'))
+            ctx2.applyCalls('Hinge', 'Hinge');
+          else
+            ctx2.applyCalls('Hinge', 'Hinge', 'Hinge');
+        });
+      }
 
-    //  Couples right of center (they look left to view center)
-    //  reverse wheel around 1.5
-    if (couplesLeft.isNotEmpty) {
-      ctx.subContext(couplesLeft, (ctx2) {
-        if (norm.endsWith('14'))
-          ctx2.applyCalls('Half Reverse Wheel Around');
-        else if (norm.endsWith('12'))
-          ctx2.applyCalls('Reverse Wheel Around');
-        else
-          ctx2.applyCalls('Reverse Wheel Around 1.5');
-      });
-    }
+      //  Couples right of center (they look left to view center)
+      //  reverse wheel around 1.5
+      if (couplesLeft.isNotEmpty) {
+        ctx.subContext(couplesLeft, (ctx2) {
+          if (norm.endsWith('14'))
+            ctx2.applyCalls('Half Reverse Wheel Around');
+          else if (norm.endsWith('12'))
+            ctx2.applyCalls('Reverse Wheel Around');
+          else
+            ctx2.applyCalls('Reverse Wheel Around 1.5');
+        });
+      }
 
-    //  Couples left of center wheel around 1.5
-    if (couplesRight.isNotEmpty) {
-      ctx.subContext(couplesRight, (ctx2) {
-        if (norm.endsWith('14'))
-          ctx2.applyCalls('Half Wheel Around');
-        else if (norm.endsWith('12'))
-          ctx2.applyCalls('Wheel Around');
-        else
-          ctx2.applyCalls('Wheel Around 1.5');
-      });
-    }
-    //  If nobody fell in any of these three categories then something's wrong
-    if (waveDancers.isEmpty && couplesLeft.isEmpty && couplesRight.isEmpty)
-      throw CallError('Unable to calculate Cast Off 3/4');
-    if ((ctx.actives - waveDancers - couplesRight - couplesLeft).isNotEmpty)
-      throw CallError('Not all dancers can Cast Off 3/4');
+      //  Couples left of center wheel around 1.5
+      if (couplesRight.isNotEmpty) {
+        ctx.subContext(couplesRight, (ctx2) {
+          if (norm.endsWith('14'))
+            ctx2.applyCalls('Half Wheel Around');
+          else if (norm.endsWith('12'))
+            ctx2.applyCalls('Wheel Around');
+          else
+            ctx2.applyCalls('Wheel Around 1.5');
+        });
+      }
+      //  If nobody fell in any of these three categories then something's wrong
+      if (waveDancers.isEmpty && couplesLeft.isEmpty && couplesRight.isEmpty)
+        throw CallError('Unable to calculate Cast Off 3/4');
+      if ((ctx.actives - waveDancers - couplesRight - couplesLeft).isNotEmpty)
+        throw CallError('Not all dancers can Cast Off 3/4');
+    });
   }
-
 }
