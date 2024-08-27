7587ba4e6a0649165638a34711f40c18ff7c2dc9
Brad Christie <brad@bradchristie.com>
2024-08-27 12:37:06 -0700

    Perk Up from inverted lines Fixes #445

--- lib/calls/c2/perk_up.dart
+++ lib/calls/c2/perk_up.dart
@@ -22,65 +22,105 @@ import '../../animated_call.dart';
 import '../../common_dart.dart';
 import '../../moves.dart';
 
-  final List<AnimatedCall> PerkUp = [
-
-    AnimatedCall('Perk Up',
-      formation:Formation('', dancers:[
-        DancerModel.fromData(gender:Gender.GIRL,x:3,y:2,angle:270),
-        DancerModel.fromData(gender:Gender.BOY,x:1,y:2,angle:90),
-        DancerModel.fromData(gender:Gender.GIRL,x:-1,y:2,angle:270),
-        DancerModel.fromData(gender:Gender.BOY,x:-3,y:2,angle:90),
-  ]),
-      from:'Right-Hand Waves',fractions:'4;2',
-      paths:[
-          Forward_4 +
-          LeadRight.changeBeats(2) +
-          Forward_4 +
-          RunRight.changeBeats(4).scale(2.0,3.0),
-
-          RunLeft.changeBeats(4) +
-          Forward_2 +
-          HingeRight.changeBeats(2) +
-          RunRight +
-          SwingRight,
-
-          Forward_4 +
-          LeadRight.changeBeats(2) +
-          RunRight.changeBeats(4).scale(2.0,3.0) +
-          Forward_4,
-
-          RunRight.changeBeats(4).scale(2.0,3.0) +
-          Forward_2 +
-          HingeRight.changeBeats(2) +
-          Forward_4.changeBeats(3) +
-          SwingRight
+final List<AnimatedCall> PerkUp = [
+  AnimatedCall('Perk Up',
+      formation: Formation('', dancers: [
+        DancerModel.fromData(gender: Gender.GIRL, x: 3, y: 2, angle: 270),
+        DancerModel.fromData(gender: Gender.BOY, x: 1, y: 2, angle: 90),
+        DancerModel.fromData(gender: Gender.GIRL, x: -1, y: 2, angle: 270),
+        DancerModel.fromData(gender: Gender.BOY, x: -3, y: 2, angle: 90),
+      ]),
+      from: 'Right-Hand Waves',
+      fractions: '4;2',
+      paths: [
+        Forward_4 + LeadRight.changeBeats(2) + Forward_4 + RunRight.changeBeats(4).scale(2.0, 3.0),
+        RunLeft.changeBeats(4) + Forward_2 + HingeRight.changeBeats(2) + RunRight + SwingRight,
+        Forward_4 + LeadRight.changeBeats(2) + RunRight.changeBeats(4).scale(2.0, 3.0) + Forward_4,
+        RunRight.changeBeats(4).scale(2.0, 3.0) +
+            Forward_2 +
+            HingeRight.changeBeats(2) +
+            Forward_4.changeBeats(3) +
+            SwingRight
+      ]),
+  AnimatedCall('Perk Up',
+      formation: Formation('Ocean Waves LH BGBG'),
+      from: 'Left-Hand Waves',
+      fractions: '4;2',
+      paths: [
+        RunLeft.changeBeats(4).scale(2.0, 3.0) +
+            Forward_2 +
+            HingeLeft.changeBeats(2) +
+            Forward_4.changeBeats(3) +
+            SwingLeft,
+        Forward_4 + LeadLeft.changeBeats(2) + RunLeft.changeBeats(4).scale(2.0, 3.0) + Forward_4,
+        RunRight.changeBeats(4) + Forward_2 + HingeLeft.changeBeats(2) + RunLeft + SwingLeft,
+        Forward_4 + LeadLeft.changeBeats(2) + Forward_4 + RunLeft.changeBeats(4).scale(2.0, 3.0)
       ]),
 
-    AnimatedCall('Perk Up',
-      formation:Formation('Ocean Waves LH BGBG'),
-      from:'Left-Hand Waves',fractions:'4;2',
-      paths:[
-          RunLeft.changeBeats(4).scale(2.0,3.0) +
-          Forward_2 +
-          HingeLeft.changeBeats(2) +
-          Forward_4.changeBeats(3) +
-          SwingLeft,
+  AnimatedCall('Perk Up',
+      formation: Formation('Inverted Lines Ends Facing Out'),
+      from: 'Inverted Lines Ends Facing Out',
+      fractions: '4;2',
+      paths: [
+        RunLeft.changeBeats(4).scale(1.5,3.0) +
+            ExtendLeft.changeBeats(2).scale(2, 2) +
+            HingeRight.skew(0,.5) +
+            RunRight +
+            SwingRight,
 
-          Forward_4 +
-          LeadLeft.changeBeats(2) +
-          RunLeft.changeBeats(4).scale(2.0,3.0) +
-          Forward_4,
+        ExtendLeft.changeBeats(2).scale(2.0,0.5) +
+            ExtendRight.changeBeats(2).scale(2.0,0.5) +
+            LeadLeft.changeBeats(2) +
+            RunLeft.changeBeats(4).scale(2, 3) +
+            ExtendLeft.changeBeats(2).scale(2.5,0.5) +
+            ExtendRight.scale(1,0.5),
 
-          RunRight.changeBeats(4) +
-          Forward_2 +
-          HingeLeft.changeBeats(2) +
-          RunLeft +
-          SwingLeft,
+        ExtendLeft.changeBeats(2).scale(2.0,0.5) +
+            ExtendRight.changeBeats(2).scale(2.0,0.5) +
+            LeadRight.changeBeats(2) +
+            RunRight.changeBeats(4).scale(3, 3) +
+            ExtendLeft.changeBeats(2).scale(2.5,0.5) +
+            ExtendRight.scale(1,0.5),
 
-          Forward_4 +
-          LeadLeft.changeBeats(2) +
-          Forward_4 +
-          RunLeft.changeBeats(4).scale(2.0,3.0)
+        RunRight.changeBeats(4).scale(2.5,3.0) +
+            Forward_2 +
+            HingeRight.skew(0, -.5) +
+            Forward_3 +
+            SwingRight
       ]),
-  ];
 
+  AnimatedCall('Perk Up',
+      formation: Formation('Inverted Lines Ends Facing In'),
+      from: 'Inverted Lines Ends Facing In',
+      fractions: '4;2',
+      paths: [
+        ExtendLeft.changeBeats(2).scale(2.0,0.5) +
+            ExtendRight.changeBeats(2).scale(2.0,0.5) +
+            LeadRight.changeBeats(2) +
+            ExtendLeft.changeBeats(2).scale(2.5,0.5) +
+            ExtendRight.scale(1,0.5) +
+            RunRight.changeBeats(4).scale(3, 3),
+
+        FlipLeft.changeBeats(4) +
+            Forward_2 +
+            HingeRight.skew(0,.5) +
+            RunRight +
+            SwingRight,
+
+
+        RunRight.changeBeats(4) +
+            ExtendLeft.changeBeats(2).scale(2,2) +
+            HingeRight.skew(0, -.5) +
+            Forward_3 +
+            SwingRight,
+
+        ExtendLeft.changeBeats(2).scale(2.0,0.5) +
+            ExtendRight.changeBeats(2).scale(2.0,0.5) +
+            LeadLeft.changeBeats(2) +
+            ExtendLeft.changeBeats(2).scale(2.5,0.5) +
+            ExtendRight.scale(1,0.5) +
+            RunLeft.changeBeats(4).scale(2, 3)
+
+      ]),
+
+];
