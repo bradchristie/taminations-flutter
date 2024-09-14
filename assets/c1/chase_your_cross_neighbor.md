dfa3989081f77531d2d07ac55810c6bcef0c70ef
Brad Christie <brad@bradchristie.com>
2024-09-13 21:02:53 -0700

    Update definitions for Chase Your Neighbor, add Chase Your Cross Neighbor with definition

---
+++ assets/c1/chase_your_cross_neighbor.md
@@ -0,0 +1,11 @@
+# Chase Your Cross Neighbor
+
+From Back-to-Back Couples only: 
+In one smooth motion, Belles U-Turn Back to the right and all
+Box Circulate; 
+Cross Your Neighbor. 
+Ends in a Left-Hand Two-Faced Line. This call feels like beginning a
+[Chase Right](../plus/chase_right.md) and blending into a 
+[Cross Your Neighbor](cross_your_neighbor.md).
+
+###### @ Copyright 1983, 1986-1988, 1995-2023 Bill Davis, John Sybalsky and CALLERLAB Inc., The International Association of Square Dance Callers. Permission to reprint, republish, and create derivative works without royalty is hereby granted, provided this notice appears. Publication on the Internet of derivative works without royalty is hereby granted provided this notice appears. Permission to quote parts or all of this document without royalty is hereby granted, provided this notice is included. Information contained herein shall not be changed nor revised in any derivation or publication.
---
+++ lib/calls/c1/chase_your_cross_neighbor.dart
@@ -0,0 +1,87 @@
+/*
+
+  Taminations Square Dance Animations
+  Copyright (C) 2024 Brad Christie
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+import '../../animated_call.dart';
+import '../../common_dart.dart';
+import '../../moves.dart';
+
+final List<AnimatedCall> ChaseYourCrossNeighbor = [
+
+  AnimatedCall('Chase Your Cross Neighbor',
+      formation:Formation('Couples Facing Out'),
+      from:'Couples Facing Out',parts:'3.7',
+      paths:[
+        UmTurnRight.skew(-2.0,0.0) +
+            LeadRight.changeBeats(3.5).scale(3.5,1.5) +
+            UmTurnRight.skew(-0.5,-0.5),
+
+        FlipRight +
+            ExtendRight.changeBeats(2).scale(2.0,1.75) +
+            SwingLeft.scale(0.75,0.75) +
+            HingeLeft.skew(0.0,-0.25)
+      ]),
+
+  AnimatedCall('Chase Your Cross Neighbor',
+      formation:Formation('Lines Facing Out'),
+      from:'Lines Facing Out',parts:'3.7',
+      paths:[
+        UmTurnRight.skew(-2.0,0.0) +
+            LeadRight.changeBeats(3.5).scale(3.5,1.5) +
+            UmTurnRight.skew(-0.5,-0.5),
+
+        FlipRight +
+            ExtendRight.changeBeats(2).scale(2.0,1.75) +
+            SwingLeft.scale(0.75,0.75) +
+            HingeLeft.skew(0.0,-0.25),
+
+        UmTurnRight.skew(-2.0,0.0) +
+            LeadRight.changeBeats(3.5).scale(3.5,1.5) +
+            UmTurnRight.skew(-0.5,-0.5),
+
+        FlipRight +
+            ExtendRight.changeBeats(2).scale(2.0,1.75) +
+            SwingLeft.scale(0.75,0.75) +
+            HingeLeft.skew(0.0,-0.25)
+      ]),
+
+  AnimatedCall('Chase Your Cross Neighbor',
+      formation:Formation('Trade By'),
+      from:'Trade By',parts:'3',
+      paths:[
+        UmTurnRight.skew(-2.0,0.0) +
+            LeadRight.changeBeats(2).scale(0.5,1.0) +
+            UmTurnRight,
+
+        FlipRight +
+            ExtendRight.scale(1.0,1.5) +
+            SwingLeft.scale(0.5,0.5) +
+            HingeLeft.scale(0.5,0.5),
+
+        UmTurnRight.skew(-2.0,0.0) +
+            LeadRight.changeBeats(2).scale(0.5,1.0) +
+            UmTurnRight,
+
+        FlipRight +
+            ExtendRight.scale(1.0,1.5) +
+            SwingLeft.scale(0.5,0.5) +
+            HingeLeft.scale(0.5,0.5)
+      ]),
+
+  ];
