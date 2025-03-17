1a7f0def4aa0da4daf48465eb81d43a21c983ac7
Brad Christie <brad@bradchristie.com>
2025-03-17 10:31:01 -0700

    Team Up from lines

--- lib/calls/c3a/team_up.dart
+++ lib/calls/c3a/team_up.dart
@@ -25,6 +25,18 @@ import '../../moves.dart';
   final List<AnimatedCall> TeamUp = [
 
     AnimatedCall('Team Up',
+        formation:Formation('Normal Lines Compact'),
+        from:'Normal Lines',
+        paths:[
+          ExtendLeft.scale(1.5,.5) +
+              HingeRight.scale(1,.5),
+          RunRight.scale(.75,1).skew(.5,0),
+          DodgeLeft.skew(.5,0),
+          ExtendLeft.scale(1.5,.5) +
+              HingeRight.scale(1,.5),
+        ]),
+
+    AnimatedCall('Team Up',
         from: 'Lines Facing Out',
         formation: Formation('Lines Facing Out'),
         numbers: ['1','8','2','7','3','6','4','5'],
@@ -53,8 +65,20 @@ import '../../moves.dart';
       ]),
 
     AnimatedCall('Team Up',
+        formation:Formation('Trade By'),
+        from:'Trade By',
+        paths:[
+          QuarterLeft.skew(0.0,1.0) +
+              QuarterRight.changeBeats(1.5).changehands(Hands.RIGHT).skew(1.0,0.0),
+          LeadRight +
+              HingeRight,
+          DodgeLeft,
+          RunRight.scale(.5,1)
+        ]),
+
+    AnimatedCall('Team Up',
       formation:Formation('Inverted Lines Ends Facing Out'),
-      from:'Inverted Lines',
+      from:'Inverted Lines Ends Facing Out',
       paths:[
           LeadLeft_2.skew(-1.0,1.0) +
           QuarterRight.changeBeats(1.5).changehands(Hands.RIGHT).skew(1.0,0.0),
@@ -68,7 +92,19 @@ import '../../moves.dart';
       ]),
 
     AnimatedCall('Team Up',
-        from: '3 and 1 Lines',
+        formation:Formation('Inverted Lines Ends Facing In'),
+        from:'Inverted Lines Ends Facing In',
+        paths:[
+          ExtendLeft.scale(2,.5) +
+              HingeRight.scale(1,.5),
+          DodgeLeft.skew(-1.0,0.0),
+          RunRight.skew(-1.0,0.0),
+          ExtendLeft.scale(2,.5) +
+              HingeRight.scale(1,.5),
+        ]),
+
+    AnimatedCall('Team Up',
+        from: '3 and 1 Lines #2',
         formation: Formation('3 and 1 Lines #2'),
         numbers: ['1','8','2','7','3','6','4','5'],
         paths: [
@@ -78,6 +114,42 @@ import '../../moves.dart';
           BackSashayLeft.skew(1, 0),
           ExtendLeft.changeBeats(2).scale(2, 0.5) +
               HingeRight.scale(1, 0.5)
+        ]),
+
+    AnimatedCall('Team Up',
+        from: '3 and 1 Lines #3',
+        formation: Formation('3 and 1 Lines #3'),
+        paths: [
+          ExtendLeft.changeBeats(2).scale(2, 0.5) +
+              HingeRight.scale(1, 0.5),
+          RunRight.scale(.5, 1).skew(1, 0),
+          RunRight.scale(.5, 1).skew(-1, 0),
+          ExtendLeft.changeBeats(2).scale(2, 0.5) +
+              HingeRight.scale(1, 0.5)
+        ]),
+
+    AnimatedCall('Team Up',
+        from: '3 and 1 Lines #6',
+        formation: Formation('3 and 1 Lines #6'),
+        paths: [
+          LeadLeft.scale(1, 3).changeBeats(2) +
+              HingeRight.skew(0, 1),
+          RunRight.scale(.5, 1).skew(1, 0),
+          RunRight.scale(.5, 1).skew(-1, 0),
+          LeadRight.scale(2, 3).changeBeats(2) +
+              HingeRight,
+        ]),
+
+    AnimatedCall('Team Up',
+        from: '3 and 1 Lines #7',
+        formation: Formation('3 and 1 Lines #7'),
+        paths: [
+          LeadLeft.scale(1, 3).changeBeats(2) +
+              HingeRight.skew(0, 1),
+          BackSashayLeft.skew(-1, 0),
+          BackSashayLeft.skew(1, 0),
+          LeadRight.scale(2, 3).changeBeats(2) +
+              HingeRight,
         ]),
 
     AnimatedCall('Team Up',
