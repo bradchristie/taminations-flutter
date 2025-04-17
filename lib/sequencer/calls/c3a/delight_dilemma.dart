a4042ea4e6a8211997adb114ecdcd2fa13e790d1
Brad Christie <brad@bradchristie.com>
2025-04-17 13:43:23 -0700

    Code for Delight / Dilemma

---
+++ lib/sequencer/calls/c3a/delight_dilemma.dart
@@ -0,0 +1,42 @@
+/*
+
+  Taminations Square Dance Animations
+  Copyright (C) 2025 Brad Christie
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+import '../common.dart';
+
+class Delight_Dilemma extends Action {
+
+  @override var level = LevelData.C3A;
+
+  Delight_Dilemma(super.name);
+
+  // TODO break centers into 4 parts
+  @override
+  void performCall(CallContext ctx) {
+    var dir = name == 'Delight' ? 'Right' : 'Left';
+    ctx.subContext(ctx.outer(4), (ctx2) {
+      ctx2.applyCalls('Face $dir Circulate Twice');
+    });
+    ctx.subContext(ctx.center(4), (ctx2) {
+      ctx2.applyFacingCouplesRule();
+      ctx2.applyCalls('Swing Slip Slip Cast Off 3/4');
+    });
+  }
+
+}
