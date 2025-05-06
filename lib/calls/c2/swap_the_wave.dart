ab607bcb9a1d72aa9f33027945d2d627e51aae1f
Brad Christie <brad@bradchristie.com>
2025-05-05 21:41:39 -0700

    Animations for Reverse Swap the Wave

--- lib/calls/c2/swap_the_wave.dart
+++ lib/calls/c2/swap_the_wave.dart
@@ -106,5 +106,89 @@ import '../../moves.dart';
 
           ExtendLeft.changeBeats(4).scale(2.0,0.5)
       ]),
+
+
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('Wave RH'),
+        from:'Right-Hand Wave',
+        paths:[
+          ExtendRight.changeBeats(4).scale(2.0,2.0),
+          SwingLeft + Forward_2
+        ]),
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('', dancers:[
+          DancerModel.fromData(gender:Gender.GIRL,x:0,y:3,angle:180),
+          DancerModel.fromData(gender:Gender.BOY,x:0,y:1,angle:0),
+        ]),
+        from:'Left-Hand Wave',
+        paths:[
+          LeadLeft.changeBeats(2).scale(0.5,3.0) +
+              LeadLeft.changeBeats(2).scale(1.0,2.5),
+          Forward_2
+        ]),
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('Ocean Waves RH BGGB'),
+        from:'Right-Hand Waves',
+        paths:[
+          ExtendRight.changeBeats(4).scale(1.0,2.0),
+
+          SwingLeft.scale(1.0,0.75) +
+              ExtendRight.scale(1.0,0.5),
+
+          SwingLeft.scale(1.0,0.75) +
+              ExtendRight.scale(1.0,0.5),
+
+          ExtendRight.changeBeats(4).scale(1.0,2.0)
+        ]),
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('Ocean Waves LH BGGB'),
+        from:'Left-Hand Waves',
+        paths:[
+          LeadLeft.changeBeats(2).scale(0.5,3.0) +
+              LeadLeft.changeBeats(2).scale(1.0,1.5),
+
+          Forward.changeBeats(2),
+
+          Forward.changeBeats(2),
+
+          LeadLeft.changeBeats(2).scale(0.5,3.0) +
+              LeadLeft.changeBeats(2).scale(1.0,1.5)
+        ]),
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('Tidal Wave RH BGGB'),
+        from:'Right-Hand Tidal Wave',
+        paths:[
+          ExtendRight.changeBeats(4).scale(2.0,0.5),
+
+          SwingLeft.scale(0.5,0.5) +
+              ExtendRight.changeBeats(2).scale(2.0,0.5),
+
+          SwingLeft.scale(0.5,0.5) +
+              ExtendRight.changeBeats(2).scale(2.0,0.5),
+
+          ExtendRight.changeBeats(4).scale(2.0,0.5)
+        ]),
+
+    AnimatedCall('Reverse Swap the Wave',
+        formation:Formation('Tidal Wave LH BGGB'),
+        from:'Left-Hand Tidal Wave',
+        paths:[
+          LeadLeft.changeBeats(2).scale(0.5,1.5) +
+              LeadLeft.changeBeats(2).scale(1.0,2.5),
+
+          ExtendLeft.changeBeats(2).scale(2.0,0.5),
+
+          ExtendLeft.changeBeats(2).scale(2.0,0.5),
+
+          LeadLeft.changeBeats(2).scale(0.5,1.5) +
+              LeadLeft.changeBeats(2).scale(1.0,2.5)
+        ]),
+
+
   ];
 
