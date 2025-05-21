12de685cf42a9bb240c9252ef7b04086847cfb66
Brad Christie <brad@bradchristie.com>
2025-05-21 11:33:57 -0700

    Replace obsolete flutter_markdown package with flutter_markdown_plus

--- lib/pages/markdown_page.dart
+++ lib/pages/markdown_page.dart
@@ -18,7 +18,7 @@
  */
 
 import 'package:flutter/material.dart' as fm;
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:flutter_markdown_plus/flutter_markdown_plus.dart';
 import 'package:provider/provider.dart' as pp;
 import 'package:url_launcher/url_launcher.dart';
 
