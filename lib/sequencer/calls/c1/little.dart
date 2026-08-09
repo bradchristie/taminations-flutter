df3feb37dfb298cf642e95609ec896c9c0aac692
Brad Christie <brad@bradchristie.com>
2026-08-09 13:05:15 -0700

    Fix for some cases of Rally, and make sure it gets marked as C-3A

--- lib/sequencer/calls/c1/little.dart
+++ lib/sequencer/calls/c1/little.dart
@@ -22,7 +22,7 @@ import '../common.dart';
 
 class Little extends Action with ButCall {
 
-  @override final level = LevelData.C1;
+  @override var level = LevelData.C1;
   @override var helplink = 'c1/scoot_and_little';
   @override var butCall = 'Peel and Trail';
 
@@ -68,12 +68,15 @@ class Little extends Action with ButCall {
 
     //  More to do for Rally
     if (name.contains('Rally')) {
+      level = LevelData.C3A;
       var isRight = ctx.outer(4).every((d) => d.isCenterRight);
       var isLeft = ctx.outer(4).every((d) => d.isCenterLeft);
       if (!isLeft && !isRight)
         throw CallError('Outer 4 not in tandem after Little');
       var roll = isRight ? 'Right' : 'Left';
+      var center4 = ctx.center(4);
       ctx.applyCalls('Center 4 $butCall While Outer 4 $roll Roll to a Wave');
+      ctx.checkCenters(centersToCheck: center4);
     }
 
   }
