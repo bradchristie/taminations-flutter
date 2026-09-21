7bffb41b47f5e8dd8ce56682a4c85f0191e4a298
Brad Christie <brad@bradchristie.com>
2026-09-20 21:45:31 -0700

    Fix sequencer level for Left Touch 1/4

--- lib/sequencer/calls/common/left.dart
+++ lib/sequencer/calls/common/left.dart
@@ -43,7 +43,6 @@ class Left extends CodedCall {
     var leftCall = ctx.findImplementor<IsLeft>(startFrom:this)
     ?? thrower<IsLeft>(CallError('Unable to find call that can apply Left'));
     leftCall.isLeft = true;
-    leftCall.raiseLevel(LevelData.PLUS);
     return;
   }
 
